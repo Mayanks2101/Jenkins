b55584820c3a7592f46574959b9c4f74dcb458ad
Mayanks2101 <131027330+Mayanks2101@users.noreply.github.com>
2026-01-27 10:31:44 +0530

    second commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,5 +2,3 @@
 # This Phython program will print Hellow World...
 print("Hello World Demo......\n")
 print("Hello World Demo......\n")
-print("Hello World Demo......\n")
-print("Hello World Demo......\n")
